70721bbfa37d32e8152364f42a41f0d8a4ab0645
Norikaz Ishii <chiether+github@gmail.com>
2015-06-11 19:13:40 +0900

    url capitalize

--- lib/new_eden/api/account.rb
+++ lib/new_eden/api/account.rb
@@ -2,15 +2,15 @@ module NewEden
   module API
     class Account < NewEden::API::Base
       def status(opts = {})
-        return api_call('/account/AccountStatus', opts)
+        return api_call('/Account/AccountStatus', opts)
       end
 
       def api_key_info(opts = {})
-        return api_call('/account/APIKeyInfo', opts)
+        return api_call('/Account/APIKeyInfo', opts)
       end
 
       def characters(opts = {})
-        return api_call('/account/Characters', opts)
+        return api_call('/Account/Characters', opts)
       end
     end
   end
--- lib/new_eden/api/map.rb
+++ lib/new_eden/api/map.rb
@@ -4,19 +4,19 @@ module NewEden
   module API
     class Map < NewEden::API::Base
       def faction_wars(opts = {})
-        return api_call('Map/FacWarSystems', opts)
+        return api_call('/Map/FacWarSystems', opts)
       end
 
       def jumps(opts = {})
-        return api_call('Map/Jumps', opts)
+        return api_call('/Map/Jumps', opts)
       end
 
       def kills(opts = {})
-        return api_call('/map/Kills', opts)
+        return api_call('/Map/Kills', opts)
       end
 
       def sovereignties(opts = {})
-        return api_call('Map/Sovereignty', opts)
+        return api_call('/Map/Sovereignty', opts)
       end
     end
   end
